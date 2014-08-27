865bc2a957dc6f2a19c05654d43351281632bf23
serge45 <Serge45497@gmail.com>
2014-08-27 23:40:20 +0800

    Moved templates folder, now use generic view

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, url
-
+from django.views.generic import ListView
+from blog.models import BlogPost
 from blog import views
 
 urlpatterns = patterns('',
-                       url(r'^$', views.index, name='index'),
+                       #url(r'^$', views.index, name='index'),
+                       url(r'^$', ListView.as_view(model=BlogPost)),
                        url(r'^(?P<post_id>\d+)/$', views.blog_content, name='content'),
 )
--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,9 @@ from blog.models import BlogPost
 
 def index(request):
     blog_post_list = BlogPost.objects.all()
-    context = {'blog_post_list' : blog_post_list}
-    return render(request, 'index.html', context)
+    context = {'blog/blogpost_list.html' : blog_post_list}
+    return render(request, 'blog/blogpost_list.html', context)
 
 def blog_content(request, post_id):
     post = get_object_or_404(BlogPost, pk=post_id)
-    return render(request, 'post_content.html', {'post' : post,})
+    return render(request, 'blog/post_content.html', {'post' : post,})
