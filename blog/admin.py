f51bf758d899553f4918ebf1b41063b6ba363a2c
serge45 <Serge45497@gmail.com>
2014-08-28 23:21:31 +0800

    Implemented categories

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from blog.models import BlogPost
+import models
 
 class BlogPostAdmin(admin.ModelAdmin):
     list_display = ('title', 'user', 'post_datetime', 'last_update_datetime', 'user')
     search_fields = ['title']
     prepopulated_fields = {"slug" : ("title", ), }
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        obj.save()
+
+admin.site.register(models.Category)
 admin.site.register(BlogPost, BlogPostAdmin)
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
 class BlogPost(models.Model):
     user = models.ForeignKey(User)
     title = models.CharField(max_length=100)
@@ -10,6 +20,7 @@ class BlogPost(models.Model):
     last_update_datetime = models.DateTimeField(auto_now=True)
     slug = models.SlugField(max_length=40, unique=True)
     site = models.ForeignKey(Site)
+    category = models.ForeignKey(Category, blank=True, null=True)
 
     class Meta:
         ordering = ["-post_datetime"]
