9aa1e582cb8dc73c03ff5a2d30bd751f419f36a0
serge45 <Serge45497@gmail.com>
2014-08-30 14:46:53 +0800

    Modified settings.py

--- sergeBlog/settings.py
+++ sergeBlog/settings.py
@@ -84,7 +84,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # Template path
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
@@ -101,14 +100,13 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-PROJECT_DIR = os.path.join(PROJECT_ROOT, '../blog')
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
-#STATICFILES_DIRS = (
-#    os.path.join(PROJECT_DIR, '/blog', 'static'),
-#)
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, 'static'),
+)
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
